434b5ee3738c3c1d79c3e0a1b2f9f9c9aff74233
alonxbadmusic <167286922+alonxbadmusic@users.noreply.github.com>
2024-04-19 04:27:26 +0530

    Update queue.py

--- modules/utils/stream/queue.py
+++ modules/utils/stream/queue.py
@@ -1,8 +1,4 @@
-# Power By @BikashHalder & @AdityaHalder 
-# Join @BikashGadgetsTech For More Update
-# Join @AdityaCheats For Hack
-# Join Our Chats @Bgt_Chat & @Adityadiscus 
-
+# Power By @VENDETTAAI
 from typing import Union
 
 from modules.config import autoclean, chatstats, userstats
