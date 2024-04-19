f5d7d66ab54752b1dfeefa339a69f1b7c0bb2a5a
alonxbadmusic <167286922+alonxbadmusic@users.noreply.github.com>
2024-04-19 11:14:46 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,7 +1,4 @@
-# Power By @BikashHalder & @AdityaHalder 
-# Join @BikashGadgetsTech For More Update
-# Join @AdityaCheats For Hack
-# Join Our Chats @Bgt_Chat & @Adityadiscus 
+# Power By @VENDETTAAI
 
 import glob
 from os.path import dirname, isfile
@@ -27,7 +24,4 @@ __all__ = ALL_MODULES + ["ALL_MODULES"]
 
 
 
-# Power By @BikashHalder & @AdityaHalder 
-# Join @BikashGadgetsTech For More Update
-# Join @AdityaCheats For Hack
-# Join Our Chats @Bgt_Chat & @Adityadiscus 
+# Power By @VENDETTAAI
