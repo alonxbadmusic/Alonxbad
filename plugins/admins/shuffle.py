85ae3998e326a52cb8c6e1fc5de21a09a0911ecd
alonxbadmusic <167286922+alonxbadmusic@users.noreply.github.com>
2024-04-19 08:43:26 +0530

    Update shuffle.py

--- plugins/admins/shuffle.py
+++ plugins/admins/shuffle.py
@@ -1,7 +1,4 @@
-# Power By @BikashHalder & @AdityaHalder 
-# Join @BikashGadgetsTech For More Update
-# Join @AdityaCheats For Hack
-# Join Our Chats @Bgt_Chat & @Adityadiscus 
+# Power By @VENDETTAAI
 
 import random
 
@@ -44,7 +41,4 @@ async def admins(Client, message: Message, _, chat_id):
 
 
 
-# Power By @BikashHalder & @AdityaHalder 
-# Join @BikashGadgetsTech For More Update
-# Join @AdityaCheats For Hack
-# Join Our Chats @Bgt_Chat & @Adityadiscus 
+# Power By @VENDETTAAI
